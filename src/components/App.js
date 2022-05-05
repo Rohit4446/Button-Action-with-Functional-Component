607f240622af3214182441194189d36bde291448
Rohit4446 <rohitbahuguna.4446@gmail.com>
2022-05-06 00:11:40 +0530

    first change in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,26 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+        const [show , setShow] = useState(false)
+  const showText = () => {
+          setShow(true)
+        
+       }
   return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
+         <> 
+         <div>
+
+           <button id="click" onClick={showText}>Click me</button>
+           {show === true &&  
+           <p idp="ara">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+           }
+        
+         </div>
+         <div id="main">
+        
+        </div>
+         </>
+    
   );
 }
 
